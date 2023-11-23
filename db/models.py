d997fdd33644de8c87e2be87038f17fd6b73e622
maariagarrcia <mrrmn@myuax.com>
2023-11-23 09:59:43 +0100

    carpeta

--- db/models.py
+++ db/models.py
@@ -22,7 +22,9 @@ class DbUser(Base):
     email = Column(String)
     hashed_password = Column(String)
 
+    # Relación uno a muchos (one-to-many) 
     file = relationship("DbFile", back_populates="user")
+    carpetas = relationship("DbCarpeta", back_populates="user")
 
     
 
@@ -35,6 +37,25 @@ class DbFile(Base):
     url = Column(String)
     tipo_url = Column(String)
 
+    # Relación muchos a uno (many-to-one) con DbUser
     user_id = Column(Integer, ForeignKey("user.id"))
     user = relationship("DbUser", back_populates="file")
 
+    # Relación muchos a uno (many-to-one) con DbCarpeta
+    carpeta_id = Column(Integer, ForeignKey("carpeta.id"))
+    carpeta = relationship("DbCarpeta", back_populates="archivos")
+
+
+class DbCarpeta(Base):
+    __tablename__ = "carpeta"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String)
+    tamaño = Column(Integer)
+    
+    # Relación muchos a uno (many-to-one) con DbUser
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("DbUser", back_populates="carpetas")
+
+    # Relación uno a muchos (one-to-many) con DbFile
+    archivos = relationship("DbFile", back_populates="carpeta")
--- router/file.py
+++ router/file.py
@@ -41,36 +41,36 @@ async def get_all(db: Session = Depends(get_db),current_user:UserAuth = Depends(
     return db_file.CrudFile.get_all(db)
 
 
+
 ALLOWED_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif", ".mp4", ".avi", ".mkv", ".pdf", ".txt", ".doc", ".docx", ".odt", ".svg", ".webp", ".jfif", ".webm", ".mov", ".wmv", ".flv", ".3gp"}
 
+def get_user_folder(user_id: int):
+    # Cambia esta función según cómo estés manejando las carpetas de los usuarios
+    # Puedes usar el ID del usuario para crear carpetas únicas para cada usuario.
+    user_folder = f"./files/users/{user_id}/"
+    os.makedirs(user_folder, exist_ok=True)
+    return user_folder
+
 @router.post("/uploadfile/")
-async def upload_file(upload_file: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
+async def upload_file(upload_file: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user), folder_name: str = ""):
     file_ext = os.path.splitext(upload_file.filename)[1]
 
     if file_ext.lower() not in ALLOWED_EXTENSIONS:
         return {"error": "Formato de archivo no permitido"}
 
-    # Genera un nombre único para el archivo
-    letters = string.ascii_letters
-    rand_str = ''.join(random.choice(letters) for i in range(6))
-    new = f'_{rand_str}.'
-    filename = new.join(upload_file.filename.rsplit('.', 1))
+    user_folder = get_user_folder(current_user.id)
 
-    if file_ext.lower() in {".png", ".jpg", ".jpeg", ".gif", ".svg", ".webp", ".jfif"}:
-        path = f'./files/images/{filename}'
-    elif file_ext.lower() in {".mp4", ".avi", ".mkv",".webm", ".mov",".wmv",".flv",".3gp"}:
-        path = f'./files/videos/{filename}'
-    elif file_ext.lower() == ".pdf":
-        path = f'./files/docs/pdf/{filename}'
-    elif file_ext.lower() in {".txt", ".doc", ".docx", ".odt"}:
-        path = f'./files/docs/text/{filename}'
-    else:
-        return {"error": "Tipo de archivo no reconocido"}
-    
-    with open(path, 'w+b') as buffer:
+    if folder_name:
+        user_folder = os.path.join(user_folder, folder_name)
+        os.makedirs(user_folder, exist_ok=True)
+
+    filename = upload_file.filename
+    path = os.path.join(user_folder, filename)
+
+    with open(path, 'wb') as buffer:
         shutil.copyfileobj(upload_file.file, buffer)
-    return {"filename": path}
 
+    return {"filename": path}
 
 
 @router.delete("/delete_file/{id}")
